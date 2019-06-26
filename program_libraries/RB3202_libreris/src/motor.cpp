fc50d353fbb06d1876663ae4cf583082af975ab1
TVavrinec <40397963+TVavrinec@users.noreply.github.com>
2019-06-26 21:47:22 +0200

    nove funkce a pinout
    -doplneni pinoutu
    -dokonceni zakladnich funkci pro motor

--- program_libraries/RB3202_libreris/src/motor.cpp
+++ program_libraries/RB3202_libreris/src/motor.cpp
@@ -1,16 +1,17 @@
 #include "RB3202_pinout.h"
 #include "motor.hpp"
 
-#define 
+#define FREGUENCY 1000
+#define MAX_PWM 2047
 
-void sed_motor_pin(gpio_num_t pin, uint8_t channel)
+void motor::sed_motor_pin(gpio_num_t pin, uint8_t channel)
 {
-    ledcSetup(channel,1000,10);
+    ledcSetup(channel,FREGUENCY,10);
     ledcAttachPin(pin,channel);
-    ledcWrite(channel,1000);
+    ledcWrite(channel,MAX_PWM);
 }
 
-void sed_motor_pins()
+void motor::sed_all_motor_pins()
 {
     sed_motor_pin(RB3202::PWM_M0, 0);
     sed_motor_pin(RB3202::PWM_M1, 1);
@@ -18,19 +19,77 @@ void sed_motor_pins()
     sed_motor_pin(RB3202::PWM_M3, 3);
 }
 
-void motor_start_working()
+void motor::motor_start_working()
 {
     digitalWrite(RB3202::SLEEP_PIN,HIGH);
 }
 
+int motor::percent_to_pwm(float percent)
+{
+    return int((percent*0.01)*MAX_PWM);
+}
+
+void motor::go_forward(bool motor, int pwm)
+{
+    if(motor)
+    {
+        ledcWrite(0,MAX_PWM);
+        ledcWrite(1,motor::percent_to_pwm(pwm));  
+    }
+    else
+    {
+        ledcWrite(2,MAX_PWM);
+        ledcWrite(3,motor::percent_to_pwm(pwm));
+    }
+}
+
+void motor::go_back(bool motor, int pwm)
+{
+    if(motor)
+    {
+        ledcWrite(0,motor::percent_to_pwm(pwm));
+        ledcWrite(1,MAX_PWM);
+    }
+    else
+    {
+        ledcWrite(2,motor::percent_to_pwm(pwm));
+        ledcWrite(3,MAX_PWM);
+    }
+}
+
+void motor::sed_pwm(bool motor, bool direction, int pwm)
+{
+    if(direction)
+        go_forward(motor,pwm);
+    else
+        go_back(motor, pwm);
+}
+
+
+
+
+
+
+
+
+
 void motor::sed_motor()
 {
-    sed_motor_pins();
+    sed_all_motor_pins();
+    motor_start_working();
 }
 
 void motor::motors_power(int power_r, int power_l)
 {
+    if(power_l > 0)
+        sed_pwm(1, 1, power_l);
+    else
+        sed_pwm(1, 0, power_l);
 
+    if(power_r > 0)
+        sed_pwm(1, 1, power_r);
+    else
+        sed_pwm(1, 0, power_r);
 }
 
 motor::~motor()
--- program_libraries/RB3202_libreris/src/motor.hpp
+++ program_libraries/RB3202_libreris/src/motor.hpp
@@ -3,13 +3,18 @@
 class motor
 {
 private:
-    void sed_motor_pin();
-    void sed_motor_pins();
+    void sed_motor_pin(gpio_num_t pin, uint8_t channel);
+    void sed_all_motor_pins();
     void motor_start_working();
+    void go_forward(bool motor, int pwm);
+    void go_back(bool motor, int pwm);
+    void sed_pwm(bool motor, bool direction, int pwm);
+
+    int percent_to_pwm(float percent);
+    
 public:
     void sed_motor();
     void motors_power(int power_0, int power_1);
-    void motor_power(bool motor, int power);
     void stop();
     void stop(bool motor);
     ~motor();
